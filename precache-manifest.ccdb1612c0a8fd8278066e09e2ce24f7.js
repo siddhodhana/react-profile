c63ce79f29226df16a6916a2f22f569febd80e4b
NRK <rravikumar34@gmail.com>
2020-05-01 18:13:22 +0530

    Updates

---
+++ precache-manifest.ccdb1612c0a8fd8278066e09e2ce24f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ceef9b464743cd5dc726e611201a5ec",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "cf9dcbd7890bf54eaee8",
+    "url": "/react-profile/static/css/main.38520a42.chunk.css"
+  },
+  {
+    "revision": "eb59cbad57825e8518e8",
+    "url": "/react-profile/static/js/2.e9810b69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-profile/static/js/2.e9810b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9dcbd7890bf54eaee8",
+    "url": "/react-profile/static/js/main.1ab5e98b.chunk.js"
+  },
+  {
+    "revision": "bb0245ea557033692c61",
+    "url": "/react-profile/static/js/runtime-main.67da613d.js"
+  }
+]);
